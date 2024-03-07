65a3ec11672f7d80295d9d5bdfa98e8179883fe6
Andika Arga Pradana <andikaarga2002@gmail.com>
2024-03-07 22:25:56 +0700

    Update soal-3.js

--- soal-3.js
+++ soal-3.js
@@ -1,23 +1,19 @@
 const getAngkaTerbesarKedua = (dataAngka) => {
-  if (!Array.isArray(dataAngka)) {
-    return "ini bukan array";
-  } else {
-    if (dataAngka.length === 0) {
-      return "array kosong";
-    } else if (dataAngka.length === 1) {
+  if (!Array.isArray(dataAngka)) return "ini bukan array";
+  if (Array.isArray(dataAngka)) {
+    if (dataAngka.length === 0) return "array kosong";
+    if (dataAngka.length === 1)
       return `datanya cuma satu, nih angkanya ${dataAngka[0]}`;
-    } else {
-      dataAngka.sort((a, b) => b - a);
-      let a = 1;
-      while (dataAngka[a - 1] === dataAngka[a]) {
-        a++;
-      }
-      return dataAngka[a];
+    dataAngka.sort((a, b) => b - a);
+    let a = 1;
+    while (dataAngka[a - 1] === dataAngka[a]) {
+      a++;
     }
+    return dataAngka[a];
   }
 };
 
-const dataAngka = [9, 9, 9, 7, 4, 3, 2, 2, 7];
+const dataAngka = [9, 9, 9, 8];
 
 console.log(getAngkaTerbesarKedua(dataAngka));
 console.log(getAngkaTerbesarKedua({}));
@@ -25,4 +21,5 @@ console.log(getAngkaTerbesarKedua([]));
 console.log(getAngkaTerbesarKedua([1]));
 console.log(getAngkaTerbesarKedua(2));
 console.log(getAngkaTerbesarKedua());
+console.log(getAngkaTerbesarKedua([2, "x"]));
 console.log(getAngkaTerbesarKedua("2"));
