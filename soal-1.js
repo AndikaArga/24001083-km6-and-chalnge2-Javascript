c3cbffb1f52d25d816ebeceda2e99e5a5f4007da
Andika Arga Pradana <andikaarga2002@gmail.com>
2024-03-08 15:48:26 +0700

    Update soal-1.js

--- soal-1.js
+++ soal-1.js
@@ -1,10 +1,12 @@
-const changeword = (selectedText, changeText, text) => {
+const changeword = (...data) => {
+  if (data.length != 3) return "data tidak sesuai";
+  const [selectedText, changeText,text] = data
   return text.replace(selectedText, changeText);
 };
 
 const kalimat1 = "Andini sangat mencintai kamu selamanya";
 const kalimat2 =
   "Gunung bromo tak akan mampu menggambarkan besarnya cintaku padamu :D";
-
 console.log(changeword("mencintai", "membenci", kalimat1));
 console.log(changeword("bromo", "semeru", kalimat2));
+
