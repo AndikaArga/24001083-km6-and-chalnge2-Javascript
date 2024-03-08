508356ce77d8108e0d1385d3bc8154954ec2a1b8
Andika Arga Pradana <andikaarga2002@gmail.com>
2024-03-08 18:14:21 +0700

    Update soalTambahan.js

--- soalTambahan.js
+++ soalTambahan.js
@@ -1,20 +1,43 @@
+const hitungFrequency = (data) => {
+  if (data === undefined) return "error : tidak ada parameter";
+  if (!Array.isArray(data)) return "error : bukan array";
+  if (data.length === 0) return "error : arraynya masih kosong";
+  if (data.length === 1) return `error : hanya ada 1 data yaitu ${data[0]}`;
+
+  let maxFrequency = 0;
+  let mostFrequentItems = {};
+
+  const frequency = {};
+
+  data.forEach((e) => {
+    if (frequency[e]) {
+      frequency[e]++;
+    } else {
+      frequency[e] = 1;
+    }
+
+    if (frequency[e] > maxFrequency) {
+      maxFrequency = frequency[e];
+      mostFrequentItems = [e];
+    } else if (frequency[e] === maxFrequency) {
+      mostFrequentItems.push(e);
+    }
+  });
+
+  return `item ${mostFrequentItems.toString()} muncul sebanyak ${maxFrequency} kali`;
+};
+
 let arr1 = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
+let arr2 = [3];
+let arr3 = [3, 3, 4, 4];
+let arr4 = [1, 2, 3, 4, 5];
 
-let start = 1;
-let temp = 0;
-let item;
+console.log(hitungFrequency(arr1));
+console.log(hitungFrequency(arr3));
+console.log(hitungFrequency(arr4));
+console.log(hitungFrequency(arr2,arr1));
 
-for (let i = 0; i < arr1.length; i++) {
-  for (let j = i; j < arr1.length; j++) {
-    if (arr1[i] == arr1[j]) {
-      temp++;
-    }
-    if (start < temp) {
-      start = temp;
-      item = arr1[i];
-    }
-  }
-  temp = 0;
-}
 
-console.log(`data ${item} sebanyak ${start} kali`);
+
+
+
