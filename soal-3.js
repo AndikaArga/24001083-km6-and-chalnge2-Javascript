74b6820baf83264cfb75db843b6089dc654884ed
Andika Arga Pradana <andikaarga2002@gmail.com>
2024-03-08 16:07:59 +0700

    Update soal-3.js

--- soal-3.js
+++ soal-3.js
@@ -1,10 +1,10 @@
 const getAngkaTerbesarKedua = (dataAngka) => {
-  if (!Array.isArray(dataAngka)) return "ini bukan array";
-  if (dataAngka.length === 0) return "array kosong";
+  if (!Array.isArray(dataAngka)) return "error : ini bukan array";
+  if (dataAngka.length === 0) return "error : array kosong";
   if (dataAngka.length === 1)
-    return `datanya cuma satu, nih angkanya ${dataAngka[0]}`;
-  if (dataAngka.some((e) => isNaN(e))) return "ada yang bukan number nih di arraynya";
-  if (dataAngka.every((e) => e === dataAngka[0])) return `angkanya sama semua nih, ${dataAngka[0]} semua`
+    return `error : datanya cuma satu, nih angkanya ${dataAngka[0]}`;
+  if (dataAngka.some((e) => isNaN(e))) return "error : ada yang bukan number nih di arraynya";
+  if (dataAngka.every((e) => e === dataAngka[0])) return `error : angkanya sama semua nih, ${dataAngka[0]} semua`
   dataAngka.sort((a, b) => b - a);
   let a = 1;
   while (dataAngka[a - 1] === dataAngka[a]) {
@@ -20,7 +20,7 @@ console.log(getAngkaTerbesarKedua({}));
 console.log(getAngkaTerbesarKedua([]));
 console.log(getAngkaTerbesarKedua([1,1,1,1,1]));
 console.log(getAngkaTerbesarKedua([1]));
-console.log(getAngkaTerbesarKedua(2));
+console.log(getAngkaTerbesarKedua(0));
 console.log(getAngkaTerbesarKedua());
 console.log(getAngkaTerbesarKedua([2, "x"]));
 console.log(getAngkaTerbesarKedua("2"));
