000b9bb1a491b107856a9180bd322399c92c2366
Andika Arga Pradana <andikaarga2002@gmail.com>
2024-03-08 19:54:53 +0700

    Update soalTambahan.js

--- soalTambahan.js
+++ soalTambahan.js
@@ -1,5 +1,6 @@
 const hitungFrequency = (...dataArray) => {
-  if(dataArray.length > 1) return "error : data yang dimasukkan terlalu banyak"
+  if (dataArray.length > 1)
+    return "error : data yang dimasukkan terlalu banyak";
   const data = dataArray[0];
   if (data === undefined) return "error : tidak ada parameter";
   if (!Array.isArray(data)) return "error : bukan array";
@@ -7,15 +8,15 @@ const hitungFrequency = (...dataArray) => {
   if (data.length === 1) return `error : hanya ada 1 data yaitu ${data[0]}`;
 
   let maxFrequency = 0;
-  let mostFrequentItems = {};
+  let mostFrequentItems;
 
   const frequency = {};
 
   data.forEach((e) => {
     if (frequency[e]) {
-      frequency[e]++;
+      frequency[e]++; //{e : 1 + 1}
     } else {
-      frequency[e] = 1;
+      frequency[e] = 1; //{e : 1}
     }
 
     if (frequency[e] > maxFrequency) {
@@ -26,7 +27,7 @@ const hitungFrequency = (...dataArray) => {
     }
   });
 
-  return `item ${mostFrequentItems.toString()} muncul sebanyak ${maxFrequency} kali`;
+  return `item ${mostFrequentItems} muncul sebanyak ${maxFrequency} kali`;
 };
 
 let arr1 = [3, "a", "a", "a", 2, 3, "a", 3, "a", 2, 4, 9, 3];
@@ -37,10 +38,4 @@ let arr4 = [1, 2, 3, 4, 5];
 console.log(hitungFrequency(arr1));
 console.log(hitungFrequency(arr3));
 console.log(hitungFrequency(arr4));
-console.log(hitungFrequency(arr2,arr1));
-console.log(hitungFrequency([]));
-
-
-
-
-
+console.log(hitungFrequency(arr2, arr1));
